13f0e9bb606c4f7bdec961c05542bb1ba1743578
Jonathan <hermankats16@gmail.com>
2024-08-28 21:00:42 +0300

    added an events model

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)  # Title of the event
+    description = models.TextField()  # Detailed description of the event
+    event_date = models.DateField()  # Date of the event
+    event_time = models.TimeField()  # Time of the event
+    location = models.CharField(max_length=100)  # Location of the event
+    image = models.ImageField(upload_to='event_images/')  # Image of the event
+
+    def __str__(self):
+        return self.title
+
--- lawanci/urls.py
+++ lawanci/urls.py
@@ -31,3 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('paypal.standard.ipn.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
